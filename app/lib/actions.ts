b01cf5e240c52d6923521b002a313602e8ae1373
Eriko <erifankobo@gmail.com>
2026-03-07 12:26:50 +0100

    added the redirect property in signIn

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -39,7 +39,10 @@ export async function authenticate(
   formData: FormData,
 ) {
   try {
-    await signIn("credentials", formData);
+    await signIn("credentials", {
+      ...Object.fromEntries(formData),
+      redirect: false,
+    });
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
